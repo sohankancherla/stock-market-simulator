9d080a10f3229f9b144707a541f398f83ffd8d93
sohankancherla <30853467+sohankancherla@users.noreply.github.com>
2023-11-11 19:46:55 -0800

    completed login code

--- app.py
+++ app.py
@@ -56,24 +56,34 @@ def history():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        # Make sure username has enough characters
+        if len(username) < 3:
+            flash("Usernames have at least 3 characters.", "danger")
+            return render_template('login.html', username=username, password=password)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+        # Make sure the username has valid characters
+        if not re.match("^[\w.]+$", username):
+            flash("Usernames only use letters, numbers, underscores and periods.", "danger")
+            return render_template('login.html', username=username, password=password)
+
+        # Make sure the password has enough characters
+        if len(password) < 5:
+            flash("Password have at least 5 characters.", "danger")
+            return render_template('login.html', username=username, password=password)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            flash("Invalid username and/or password.", "danger")
+            return render_template('login.html', username=username, password=password)
+
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
